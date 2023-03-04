92d872893eafa0966f8bd2dcdfa14e8ff13d3a12
Piotr Kamecki <piotrkamecki8@gmail.com>
2023-03-04 18:08:13 +0100

    add image sending function

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/images/uploads', express.static('uploads'));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -31,6 +34,27 @@ app.get('/info', (req, res) => {
   
 app.get('/history', (req, res) => {
   res.render('history');
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message && req.file) {
+    res.render('contact', { isSent: true, fileName: req.file.originalname });
+  }
+  else {
+    res.render('contact', { isError: true })
+  }
 });
 
 app.use((req, res) => {
